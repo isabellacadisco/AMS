33c6a8b1addbbeeb063ac301319a8cd1823bf4a9
mahnaztaheri <128820923+mahnaztaheri@users.noreply.github.com>
2023-03-24 15:17:26 +0100

    Add files via upload

---
+++ mahnaz/untitled0.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+ds = pd.read_csv("D:/wdi2018.csv")
+
+#ds.summary()
+#ds.info()
+#ds.columns
+
+#ds.isna()
+v=[]
+v = pd.DataFrame([v])
+k=ds.isna().sum()
+i = 0
+for i in range(len(ds.columns)):
+    if ((k[i] / len(ds.iloc[:,i])) * 100) > 7:
+          v.loc[0,i]=i
+for i in reversed(range(len(v.columns))):
+    ds=ds.drop(ds.columns[v.iloc[0,i]], axis=1)
+
+ds2 = pd.read_csv('D:/income.csv')
+
+ds.loc[:,140]=0
+for i in range(len(ds)):
+    for j in range(len(ds2)):
+        if ds2.iloc[j,0] == ds.iloc[i,3]:
+            ds.loc[i,140]= ds2.iloc[j,2]
+            
+            
+
+       
